f8c61c55370b506d46b064a65203b3537129c64d
EC2 Default User <ec2-user@ip-172-31-24-97.ap-southeast-1.compute.internal>
2016-06-08 17:10:01 +0800

    inital

---
+++ puller.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 13:07 08/06/2016
+# REM dl prices files from web
+PREV_DAYS=30
+homeDir=/home/ec2-user
+datDir=${homeDir}/data/dat
+
+#debug
+echo started puller.sh on: `date`
+
+#mkdir for datDir
+if [ ! -d $datDir ]; then
+	mkdir $datDir
+fi 
+
+#get file from server
+for i in `seq 1 $PREV_DAYS`
+do
+	#iterate dates
+	fdate=`date -d "-${i}days" +%F`
+	#check file exists, else dl files
+	if [ ! -f ${datDir}/SESprice_${fdate}.dat ]; then
+		wget "http://infopub.sgx.com/Apps?A=COW_Prices_Content&B=SecuritiesHistoricalPrice&F=4451&G=SESprice.dat&H=${fdate}" -O ${datDir}/SESprice_${fdate}.dat
+	else
+		echo $fdate found
+	fi
+done 
+
+#echo "http://infopub.sgx.com/Apps?A=COW_Prices_Content&B=SecuritiesHistoricalPrice&F=4451&G=SESprice.dat&H=$(date +%F)" -O ${datDir}SESprice_$(date --date='-$i day' +%F).dat
+
+#wget "http://infopub.sgx.com/Apps?A=COW_Prices_Content&B=SecuritiesHistoricalPrice&F=4704&G=SESprice.dat&H=2016-06-07"
---
+++ sedproc.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# 16:01 08/06/2016
+# REM proc to clean spaces from dl files
+
+homeDir=/home/ec2-user
+datDir=${homeDir}/data/dat
+texDir=${homeDir}/data/tex
+files=*.dat
+dlm=;
+
+#debug
+echo started sedproc.sh on: `date`
+#mkdir for datDir
+if [ ! -d $datDir ]; then
+	mkdir $datDir
+fi 
+#mkdir for texDir
+if [ ! -d $texDir ]; then
+	mkdir $texDir
+fi 
+
+cd ${datDir}
+for file in ${datDir}/*.dat; do
+	# echo $file
+	
+	#remove front filepath
+	name=${file##*/}
+	# echo $name
+	
+	#remove ext
+	#base=${name%.dat}
+	#echo $base
+	
+	#	REM remove double space/tab
+	# echo sed-ing $file
+	sed 's/  //g' <$file >tmp.01
+	sed 's/	//g' <tmp.01 >tmp.02
+
+	# #	REM remove space between delimiters
+	sed 's/$dlm /$dlm/g' <tmp.02 >tmp.03
+	sed 's/ $dlm/$dlm/g' <tmp.03 >tmp.04
+
+	# #	REM remove space/tab from begining/end --^ *--(escaped ^)(begining ^)[(space/tab)](multiple *)(end $)
+	# sed 's/^^[ \t]*//g' <tmp.04 >tmp.05
+	# sed 's/[ \t]*$//g' <tmp.05 >tmp.00
+	
+	mv tmp.04 "${texDir}/${name%.dat}.tex"
+	echo processed ${texDir}/${name%.dat}.tex
+	
+done
+
+rm tmp.00
+rm tmp.01
+rm tmp.02
+rm tmp.03
+#rm tmp.04
+#rm tmp.05
+
+echo completed sedproc.sh on: `date`
